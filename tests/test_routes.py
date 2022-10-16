5e8dc87f44c4bfe7813421ff1c07d2cdcc2c4d96
Victoria Zauner <60822625+VickyZauner@users.noreply.github.com>
2022-10-16 18:35:47 -0400

    Update test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -37,6 +37,20 @@ class TestYourResourceServer(TestCase):
     def tearDown(self):
         """ This runs after each test """
         pass
+    
+    def _create_recommendation(self, count):
+        """Factory method to create recommendations in bulk"""
+        recommendations = []
+        for _ in range(count):
+            test_recommendation = RecommendationFactory()
+            response = self.client.post(BASE_URL, json=test_pet.serialize())
+            self.assertEqual(
+                response.status_code, status.HTTP_201_CREATED, "Could not create test recommendation"
+            )
+            new_recommendation = response.get_json()
+            test_recommendation.id = new_recommendation["id"]
+            recommendations.append(test_recommendation)
+        return recommendations
 
     ######################################################################
     #  P L A C E   T E S T   C A S E S   H E R E
@@ -46,3 +60,39 @@ class TestYourResourceServer(TestCase):
         """ It should call the home page """
         resp = self.app.get("/")
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        
+    
+     
+      
+      
+      def test_create_pet(self):
+        """It should Create a new Recommendation"""
+        test_recommendations = RecommendationsFactory()
+        logging.debug("Test Recommendation: %s", test_recommendation.serialize())
+        response = self.client.post(BASE_URL, json=test_recommendation.serialize())
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        # Make sure location header is set
+        location = response.headers.get("Location", None)
+        self.assertIsNotNone(location)
+
+        # Check the data is correct
+        new_recommendation = response.get_json()
+        self.assertEqual(new_recommendation["id"], test_recommendation.id)
+        self.assertEqual(new_recommendation["name"], test_recommendation.name)
+        self.assertEqual(new_recommendation["recommendId"], test_recommendation.recommendId)
+        self.assertEqual(new_recommendation["recommendedName"], test_recommendation.recommendedName)
+        self.assertEqual(new_recommendation["numberOfLikes"], test_recommendation.numberOfLikes)
+        self.assertEqual(new_recommendation["recommendationType"], test_recommendation.recommendationType)
+
+        # Check that the location header was correct
+        response = self.client.get(location)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        new_recommendation = response.get_json()
+        self.assertEqual(new_recommendation["id"], test_recommendation.id)
+        self.assertEqual(new_recommendation["name"], test_recommendation.name)
+        self.assertEqual(new_recommendation["recommendId"], test_recommendation.recommendId)
+        self.assertEqual(new_recommendation["recommendedName"], test_recommendation.recommendedName)
+        self.assertEqual(new_recommendation["numberOfLikes"], test_recommendation.numberOfLikes)
+        self.assertEqual(new_recommendation["recommendationType"], test_recommendation.recommendationType)
+
