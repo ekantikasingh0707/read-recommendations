fc0b17a43f7445df9a2b1dfe6e0bc5fbf7ff8a3a
Dhruv Shetty <dhruvshetty3@gmail.com>
2022-10-17 11:44:56 -0400

    Crate Factories, still not working

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -10,8 +10,14 @@ import logging
 from unittest import TestCase
 from unittest.mock import MagicMock, patch
 from service import app
-from service.models import db
+from service.models import db, init_db, Recommendation
+from tests.factories import RecommendationsFactory
 from service.common import status  # HTTP Status Codes
+
+DATABASE_URI = os.getenv(
+    "DATABASE_URI", "postgresql://postgres:postgres@localhost:5432/testdb"
+)
+BASE_URL = "/recommendations"
 
 
 ######################################################################
@@ -23,27 +29,35 @@ class TestYourResourceServer(TestCase):
     @classmethod
     def setUpClass(cls):
         """ This runs once before the entire test suite """
-        pass
+        app.config["TESTING"] = True
+        app.config["DEBUG"] = False
+        # Set up the test database
+        app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
+        app.logger.setLevel(logging.CRITICAL)
+        init_db(app)
+
 
     @classmethod
     def tearDownClass(cls):
         """ This runs once after the entire test suite """
-        pass
+        db.session.close()
 
     def setUp(self):
         """ This runs before each test """
-        self.app = app.test_client()
+        self.client = app.test_client()
+        db.session.query(Recommendation).delete()  # clean up the last tests
+        db.session.commit()
 
     def tearDown(self):
         """ This runs after each test """
-        pass
+        db.session.remove()
     
     def _create_recommendation(self, count):
         """Factory method to create recommendations in bulk"""
         recommendations = []
         for _ in range(count):
-            test_recommendation = RecommendationFactory()
-            response = self.client.post(BASE_URL, json=test_pet.serialize())
+            test_recommendation = RecommendationsFactory()
+            response = self.client.post(BASE_URL, json=test_recommendation.serialize())
             self.assertEqual(
                 response.status_code, status.HTTP_201_CREATED, "Could not create test recommendation"
             )
@@ -61,13 +75,9 @@ class TestYourResourceServer(TestCase):
         resp = self.app.get("/")
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         
-    
-     
-      
-      
     def test_create_recommendation(self):
         """It should Create a new Recommendation"""
-        test_recommendations = RecommendationsFactory()
+        test_recommendation = RecommendationsFactory()
         logging.debug("Test Recommendation: %s", test_recommendation.serialize())
         response = self.client.post(BASE_URL, json=test_recommendation.serialize())
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
@@ -86,7 +96,7 @@ class TestYourResourceServer(TestCase):
         self.assertEqual(new_recommendation["number_of_likes"], test_recommendation.number_of_likes)
 
         # Check that the location header was correct
-        '''response = self.client.get(location)
+        """response = self.client.get(location)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         new_recommendation = response.get_json()
         self.assertEqual(new_recommendation["id"], test_recommendation.id)
@@ -94,5 +104,5 @@ class TestYourResourceServer(TestCase):
         self.assertEqual(new_recommendation["recommendId"], test_recommendation.recommendId)
         self.assertEqual(new_recommendation["recommendedName"], test_recommendation.recommendedName)
         self.assertEqual(new_recommendation["numberOfLikes"], test_recommendation.numberOfLikes)
-        self.assertEqual(new_recommendation["recommendationType"], test_recommendation.recommendationType)'''
+        self.assertEqual(new_recommendation["recommendationType"], test_recommendation.recommendationType)"""
 
